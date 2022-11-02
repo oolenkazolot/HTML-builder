116d2a02b055d39035f0b6c688ae97a2e68ccdcd
oolenkazolot <oolenka.zolot@gmail.com>
2022-11-02 10:33:22 +0100

    feat: implement folder copying (task 04)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
 
+const newDirectoryPath = "04-copy-directory/files-copy";
+const directoryPath = "04-copy-directory/files";
+
+const fs = require('fs');
+
+fs.mkdir(newDirectoryPath, { recursive: true }, (err) => {
+
+  if (err) {
+    console.error(err)
+    return;
+  }
+
+})
+
+fs.readdir(directoryPath, (err, files) => {
+
+  if (err) {
+    console.error(err)
+    return;
+  }
+
+  files.forEach(element => {
+    fs.copyFile(`${directoryPath}/${element}`, `${newDirectoryPath}/${element}`, err => {
+
+      if(err) throw err; // failed to copy file
+
+    });
+
+  });
+})
